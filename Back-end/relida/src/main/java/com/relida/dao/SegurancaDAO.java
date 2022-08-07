d45cd2afb3855929352048ac7f70611b55b52019
BrunoJava-2021 <bruno.alexandre.java@gmail.com>
2022-08-07 15:00:17 -0300

    Bruno

--- Back-end/relida/src/main/java/com/relida/dao/SegurancaDAO.java
+++ Back-end/relida/src/main/java/com/relida/dao/SegurancaDAO.java
@@ -1,5 +1,14 @@
 package com.relida.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.relida.domain.PerguntaDeSeguranca;
+
 public interface SegurancaDAO {
 
+	public interface AnuncioDAO extends JpaRepository<PerguntaDeSeguranca, Integer> {
+		
+	}
+
+	
 }
